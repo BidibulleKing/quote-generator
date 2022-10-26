fd05ea327def774ced4763ca7ca00959c1fe4af6
BidibulleKing <remy.delepaule@gmail.com>
2022-10-26 23:58:15 +0200

    Exercice terminé

--- js/app.js
+++ js/app.js
@@ -6,45 +6,126 @@
 
 // On place notre code dans un module
 const app = {
-    // Propriété "counter"
-    currentQuoteIndex: 0,
-    // Méthode appelée au chargement du DOM
-    init: function() {
-        // attache la méthode app.handleClickOnDisplayAddFormButton à l'évènement "click" sur le bouton "ajouter une citation"
-        document.getElementById('btnDisplayAddForm').addEventListener('click', app.handleClickOnDisplayAddFormButton);
+  // Propriété "counter"
+  currentQuoteIndex: 0,
+  // Méthode appelée au chargement du DOM
+  init: () => {
+    // attache la méthode app.handleClickOnDisplayAddFormButton à l'évènement "click" sur le bouton "ajouter une citation"
 
-        // TODO afficher la première citation de fail
+    // TODO afficher la première citation de fail
+    app.displayCurrentQuote(
+      quotes[app.currentQuoteIndex].quote,
+      quotes[app.currentQuoteIndex].author
+    );
 
-        // TODO attacher la méthode app.handleClickOnNextButton à l'évènement "click" sur le bouton "next" (id="nav-next")
-    },
-    // Méthode gérant le click pour afficher le form d'ajout
-    handleClickOnDisplayAddFormButton: function(evt) {
-        console.log('click to display form');
+    // TODO attacher la méthode app.handleClickOnNextButton à l'évènement "click" sur le bouton "next" (id="nav-next")
+    const nextButton = document.querySelector("#nav-next");
+    const prevButton = document.querySelector("#nav-prev");
+    const firstButton = document.querySelector("#nav-first");
+    const lastButton = document.querySelector("#nav-last");
 
-        document.getElementById('divAddQuote').classList.remove('d-none');
-    },
-    // Méthode permettant de modifier le DOM pour afficher la quote "courante"
-    displayCurrentQuote: function() {
-        // TODO se baser sur app.currentQuoteIndex pour afficher la quote "courante"
-    },
-    // Je crée une méthode dédiée à la gestion du click sur le bouton "Next"
-    handleClickOnNextButton: function() {
-      console.log('click on next');
+    const card = document.querySelector(".card-body");
+    const form = document.querySelector("#addQuoteForm");
 
+    nextButton.addEventListener("click", () => {
+      app.handleClickOnNextButton();
+      app.displayCurrentQuote(
+        quotes[app.currentQuoteIndex].quote,
+        quotes[app.currentQuoteIndex].author
+      );
+    });
+
+    prevButton.addEventListener("click", () => {
+      app.handleClickOnPrevButton();
+      app.displayCurrentQuote(
+        quotes[app.currentQuoteIndex].quote,
+        quotes[app.currentQuoteIndex].author
+      );
+    });
+
+    firstButton.addEventListener("click", () => {
+      app.handleClickOnFirstButton();
+      app.displayCurrentQuote(
+        quotes[app.currentQuoteIndex].quote,
+        quotes[app.currentQuoteIndex].author
+      );
+    });
+
+    lastButton.addEventListener("click", () => {
+      app.handleClickOnLastButton();
+      app.displayCurrentQuote(
+        quotes[app.currentQuoteIndex].quote,
+        quotes[app.currentQuoteIndex].author
+      );
+    });
+
+    if (card) {
+      document
+        .getElementById("btnDisplayAddForm")
+        .addEventListener("click", app.handleClickOnDisplayAddFormButton);
     }
-  };
-  
-  // Appel "synchronisé" de la méthode init
-  // app.init();
-  
-  // Permet d'exécuter notre code une fois le DOM chargé
-  // => lorsque l'event DOMContentLoaded survient => la méthode app.init est appelée
-  // donc app.init n'est pas exécuter lorsque JS lit cette ligne de code
-  document.addEventListener('DOMContentLoaded', app.init); // ici, ne jamais mettre les (), sinon, la fonction/méthode sera aussitôt exécutée
-  
-  // Attention à la syntaxe, on ne doit pas mettre les () après la fonction, sinon elle est appelée aussitôt
-  // Explications :
-  // envoie de l'eau, au lancement du détecteur
-  // document.addEventListener('fuméeDetectée', envoyerDeLeau());
-  // Lorsque de la fumée sera détectée, envoie de l'eau
-  // document.addEventListener('fuméeDetectée', envoyerDeLeau);
+
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const inputQuote = form.querySelector("#input-quote").value;
+      const inputAuthor = form.querySelector("#input-author").value;
+
+      quotes.push({
+        quote: inputQuote,
+        author: inputAuthor,
+      });
+
+      form.reset();
+    });
+  },
+  // Méthode gérant le click pour afficher le form d'ajout
+  handleClickOnDisplayAddFormButton: (evt) => {
+    document.getElementById("divAddQuote").classList.remove("d-none");
+  },
+  // Méthode permettant de modifier le DOM pour afficher la quote "courante"
+  displayCurrentQuote: (quoteText, authorName) => {
+    // TODO se baser sur app.currentQuoteIndex pour afficher la quote "courante"
+    const template = document.querySelector("#card-template");
+    const clone = document.importNode(template.content, true);
+
+    const quote = clone.querySelector("#quote");
+    const author = clone.querySelector("#author");
+
+    const card = document.querySelector(".card");
+
+    quote.textContent = quoteText;
+    author.textContent = authorName;
+    card.replaceWith(clone);
+  },
+  // Je crée une méthode dédiée à la gestion du click sur le bouton "Next"
+  handleClickOnNextButton: () => {
+    app.currentQuoteIndex < quotes.length - 1
+      ? (app.currentQuoteIndex += 1)
+      : null;
+  },
+  handleClickOnPrevButton: () => {
+    app.currentQuoteIndex > 0 ? (app.currentQuoteIndex -= 1) : null;
+  },
+  handleClickOnFirstButton: () => {
+    app.currentQuoteIndex = 0;
+  },
+  handleClickOnLastButton: () => {
+    app.currentQuoteIndex = quotes.length - 1;
+  },
+};
+
+// Appel "synchronisé" de la méthode init
+// app.init();
+
+// Permet d'exécuter notre code une fois le DOM chargé
+// => lorsque l'event DOMContentLoaded survient => la méthode app.init est appelée
+// donc app.init n'est pas exécuter lorsque JS lit cette ligne de code
+document.addEventListener("DOMContentLoaded", app.init); // ici, ne jamais mettre les (), sinon, la fonction/méthode sera aussitôt exécutée
+
+// Attention à la syntaxe, on ne doit pas mettre les () après la fonction, sinon elle est appelée aussitôt
+// Explications :
+// envoie de l'eau, au lancement du détecteur
+// document.addEventListener('fuméeDetectée', envoyerDeLeau());
+// Lorsque de la fumée sera détectée, envoie de l'eau
+// document.addEventListener('fuméeDetectée', envoyerDeLeau);
